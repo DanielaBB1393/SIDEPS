f4ea75ff1bbba11e21c6539f7a9fab52c26b7e9d
Danielabb1393 <dannybb1393@gmail.com>
2020-11-12 20:43:05 -0600

    Unir proyecto

---
+++ SIDEPS/AccesoDatos/Implementacion/UsuariosAD.cs
@@ -0,0 +1,34 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos.Implementacion
+{
+    public class UsuariosAD
+    {
+        private SIDEPSEntities gobjContextoSIDEPS;
+        public UsuariosAD(SIDEPSEntities _objContextoSIDEPS)
+        {
+            this.gobjContextoSIDEPS = _objContextoSIDEPS;
+        }
+        public List<SIDEPS_07REGUSRO> ListaUsuarios()
+        {
+            List<SIDEPS_07REGUSRO> lobjRespuesta = new List<SIDEPS_07REGUSRO>();
+
+            try
+            {
+                lobjRespuesta = gobjContextoSIDEPS.SIDEPS_07REGUSRO.ToList();
+
+        }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return lobjRespuesta;
+        }
+
+    }
+}
