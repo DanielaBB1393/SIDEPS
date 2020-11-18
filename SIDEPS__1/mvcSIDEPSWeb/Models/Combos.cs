2e1a95150d7bfd6c239167d7a7ed3961439e86e4
Danielabb1393 <dannybb1393@gmail.com>
2020-11-17 21:16:58 -0600

    Modificacion de diaconias

---
+++ SIDEPS__1/mvcSIDEPSWeb/Models/Combos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvcSIDEPSWeb.Models
+{
+    public static class Combos
+    {
+       public static List<string> EstadosDiaconias = new List<string>()
+        {
+            "ACTIVO","INACTIVO"
+        };/*
+        public static List<Canton> Cantones = new List<Canton>()
+        {
+            new Canton{id=1,nombre="Alajuela"},
+            new Canton{id=2,nombre="San Jose"},
+            new Canton{id=null,nombre=" "}
+
+        };*/
+        
+    }
+   /*public class Canton
+    {
+        public int ? id { get; set; }
+        public string  nombre { get; set; }
+    };*/
+}
