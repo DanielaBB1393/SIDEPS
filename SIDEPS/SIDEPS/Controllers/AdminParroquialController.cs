e4726482c5695aa0c769dcfb1e5290d48c0b5d7f
Danielabb1393 <dannybb1393@gmail.com>
2020-12-06 13:54:02 -0600

    menu

---
+++ SIDEPS/SIDEPS/Controllers/AdminParroquialController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIDEPS.Controllers
+{
+    public class AdminParroquialController : Controller
+    {
+        // GET: AdminParroquial
+        public ActionResult MenuParroquial()
+        {
+            return View();
+        }
+    }
+}
