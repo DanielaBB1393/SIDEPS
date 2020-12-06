9e4140fdbeb9c6aa150b83491fbbe79d122c0d5c
Danielabb1393 <dannybb1393@gmail.com>
2020-12-06 15:24:34 -0600

    Auto stash before merge of "Desarrollo" and "origin/Desarrollo"

---
+++ SIDEPS/SIDEPS/Controllers/MenuCasosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIDEPS.Controllers
+{
+    public class MenuCasosController : Controller
+    {
+        // GET: MenuCasos
+        public ActionResult MenuCasos()
+        {
+            return View();
+        }
+    }
+}
---
+++ SIDEPS/SIDEPS/Controllers/MenuColaboradorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIDEPS.Controllers
+{
+    public class MenuColaboradorController : Controller
+    {
+        // GET: MenuColaborador
+        public ActionResult MenuControlador()
+        {
+            return View();
+        }
+    }
+}
