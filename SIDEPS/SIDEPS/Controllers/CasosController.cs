66556364e60a51673a5f9d1d102f52f4f205768c
Danielabb1393 <dannybb1393@gmail.com>
2020-12-10 10:43:53 -0600

    Casos incompletos

--- SIDEPS/SIDEPS/Controllers/CasosController.cs
+++ SIDEPS/SIDEPS/Controllers/CasosController.cs
@@ -445,7 +445,39 @@ namespace SIDEPS.Controllers
 
             return View(modelo);
         }
+        public ActionResult CasoIncompleto()
+        {
+            string cedulaUsuario = TempData[Combos._CEDULAUSUARIO].ToString();
+            TempData.Keep();
 
+            var modelo = new List<HistoricoCaso_M>();
+
+            using (var svc = new ServiciosWCFClient())
+            {
+                int diaconia = svc.conUsuarioXCedula(cedulaUsuario).CODDIAC04.GetValueOrDefault();
+
+                var resultado = svc.SP_Con_HistoricoCasos(diaconia).Where(caso => caso.ESTCASO25.Equals(Combos.CASO_INCOMPLETO));
+                foreach (var item in resultado)
+                {
+                    var registro = new HistoricoCaso_M();
+                    registro.CODCASO25 = item.CODCASO25;
+                    registro.CEDPERS13 = item.CEDPERS13;
+                    registro.CEDUSRO07 = item.CEDUSRO07;
+                    registro.FEICASO25 = item.FEICASO25;
+                    registro.FEFCASO25 = item.FEFCASO25;
+                    registro.DESCASO25 = item.DESCASO25;
+                    registro.OPICASO25 = item.OPICASO25;
+                    registro.ESTCASO25 = item.ESTCASO25;
+                    registro.NOMUSRO07 = item.NOMUSRO07;
+                    registro.PAPUSRO07 = item.PAPUSRO07;
+                    registro.NOMPERS13 = item.NOMPERS13;
+                    registro.PAPPERS13 = item.PAPPERS13;
+                    modelo.Add(registro);
+                }
+            }
+
+            return View(modelo);
+        }
         public ActionResult ValidarCasoMantenimiento()
         {
             string cedulaUsuario = TempData[Combos._CEDULAUSUARIO].ToString();
